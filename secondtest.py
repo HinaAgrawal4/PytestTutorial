a677d2f38a2846bd2ac28aa235d03d3f4d2ca664
HinaAgrawal <hina.agrawal010@gmail.com>
2025-08-01 15:10:02 +0530

    Test

---
+++ secondtest.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver=webdriver.Chrome()
+driver.get("https://www.salesforce.com/in/form/signup/freetrial-salesforce-starter/?d=jumbo1-btn-ft")
