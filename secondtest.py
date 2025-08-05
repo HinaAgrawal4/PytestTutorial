ac3b889c4a0a8c79dff4f23b23ed9ebd27496ff6
HinaAgrawal <hina.agrawal010@gmail.com>
2025-08-05 11:09:15 +0530

    Waits

--- secondtest.py
+++ secondtest.py
@@ -4,3 +4,4 @@ from selenium.webdriver.support.select import Select
 
 driver=webdriver.Chrome()
 driver.get("https://www.salesforce.com/in/form/signup/freetrial-salesforce-starter/?d=jumbo1-btn-ft")
+dropdown=Sele
--- selecttest.py
+++ selecttest.py
@@ -3,6 +3,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver= webdriver.Chrome()
 driver.get("https://www.salesforce.com/in/form/signup/freetrial-salesforce-starter/?d=jumbo1-btn-ft")
@@ -11,4 +13,6 @@ ddl=driver.find_element(By.NAME,"UserTitle")
 dropdown=Select(ddl)
 dropdown.select_by_index(2)
 print(dropdown)
-time.sleep(5)
+#driver.implicitly_wait(10)
+wait= WebDriverWait(driver,10)
+element=wait.until(EC.presence_of_element_located(By.NAME('UserTitle')))
